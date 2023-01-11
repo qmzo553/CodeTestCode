936ae5e6f72ea0228237903f9aefeece5e66af4d
박희준pc <pc@172.30.1.53>
2023-01-11 21:27:03 +0900

    LargeNumber

---
+++ CodingTest/Greedy/GreedyLargeNumber.java
@@ -0,0 +1,34 @@
+package Greedy;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class GreedyLargeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		int first = arr[n - 1];
+		int second = arr[n - 2];
+		
+		int cnt = (m / (k + 1)) * k;
+		cnt += m % (k + 1);
+		
+		int result = 0;
+		result += cnt * first;
+		result += (m - cnt) * second;
+		
+		System.out.println(result);
+	} 
+
+}
