7ff7991e8321c2c3e1f32c225cc098f1d4e522d1
박희준pc <pc@172.30.1.53>
2023-01-16 22:57:43 +0900

    GreedyCard

--- CodingTest/Greedy/GreedyCard.java
+++ CodingTest/Greedy/GreedyCard.java
@@ -1,9 +1,27 @@
 package Greedy;
 
+import java.util.*;
+
 public class GreedyCard {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int result = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int min_value = 10001;
+			for(int j = 0; j < m; j++) {
+				int x = sc.nextInt();
+				min_value = Math.min(min_value, x);
+			}
+			
+			result = Math.max(result, min_value);
+		}
+		
+		System.out.println(result);
 
 	}
 
