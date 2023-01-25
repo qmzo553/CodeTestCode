8b914e85dd34a534fd426b3db21a74507bea0824
박희준pc <pc@172.30.1.53>
2023-01-25 22:21:02 +0900

    DFSBFSFactorial

---
+++ CodingTest/DFSBFS/DFSBFSFactorial.java
@@ -0,0 +1,29 @@
+package DFSBFS;
+
+import java.util.*;
+
+public class DFSBFSFactorial {
+
+	// 반복적으로 구현
+	public static int factorialIterative(int n) {
+		int result = 1;
+		for(int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		return result;
+	}
+	
+	// 재귀적으로 구현
+	public static int factorialRecursive(int n) {
+		if(n <= 1) return 1;
+		return n * factorialRecursive(n - 1);
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println("반복적으로 구현 : " + factorialIterative(5));
+		System.out.println("재귀적으로 구현 : " + factorialRecursive(5));
+
+	}
+
+}
