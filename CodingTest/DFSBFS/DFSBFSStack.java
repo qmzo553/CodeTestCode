a1379e74bda9c93f8b2efa0c4d6922242f281459
박희준pc <pc@172.30.1.53>
2023-01-25 22:18:58 +0900

    DFSBFSStack

---
+++ CodingTest/DFSBFS/DFSBFSStack.java
@@ -0,0 +1,26 @@
+package DFSBFS;
+
+import java.util.*;
+
+public class DFSBFSStack {
+
+	public static void main(String[] args) {
+		Stack<Integer> s = new Stack<>();
+		
+		s.push(5);
+		s.push(2);
+		s.push(3);
+		s.push(7);
+		s.pop();
+		s.push(1);
+		s.push(4);
+		s.pop();
+		
+		while(!s.empty()) {
+			System.out.println(s.peek());
+			s.pop();
+		}
+
+	}
+
+}
