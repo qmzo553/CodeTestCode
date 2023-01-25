b904b71525328b0c543fef8bc13555b92dc19bfc
박희준pc <pc@172.30.1.53>
2023-01-25 22:21:26 +0900

    DFSBFSAdjacencyMatrix

---
+++ CodingTest/DFSBFS/DFSBFSAdjacencyMatrix.java
@@ -0,0 +1,26 @@
+package DFSBFS;
+
+import java.util.*;
+
+public class DFSBFSAdjacencyMatrix {
+	
+	public static final int INF = 999999999;
+	
+	public static int[][] graph = {
+			{0, 7, 5},
+			{7, 0, INF},
+			{5, INF, 0}
+	};
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.println(graph[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
