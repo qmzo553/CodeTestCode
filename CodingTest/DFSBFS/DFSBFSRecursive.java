55a5ef06e19b6cafe928ac2aabcdd8982a5c7512
박희준pc <pc@172.30.1.53>
2023-01-25 22:19:38 +0900

    DFSBFSRecursive

---
+++ CodingTest/DFSBFS/DFSBFSRecursive.java
@@ -0,0 +1,16 @@
+package DFSBFS;
+
+import java.util.*;
+
+public class DFSBFSRecursive {
+	
+	public static void recursiveFunction() {
+		System.out.println("재귀 함수를 호출합니다.");
+		recursiveFunction();
+	}
+	public static void main(String[] args) {
+		recursiveFunction();
+
+	}
+
+}
