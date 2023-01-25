924463a5d501e216c4cd9218d1253d3914ed131a
박희준pc <pc@172.30.1.53>
2023-01-25 22:19:15 +0900

    DFSBFSQueue

---
+++ CodingTest/DFSBFS/DFSBFSQueue.java
@@ -0,0 +1,24 @@
+package DFSBFS;
+
+import java.util.*;
+
+public class DFSBFSQueue {
+
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		
+		q.offer(5);
+		q.offer(2);
+		q.offer(3);
+		q.offer(7);
+		q.poll();
+		q.offer(1);
+		q.offer(4);
+		q.poll();
+
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
+	}
+
+}
