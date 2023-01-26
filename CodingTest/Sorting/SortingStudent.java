ca276b7d3f4ce037cae3757f485710d8c576c049
박희준pc <pc@172.30.1.53>
2023-01-26 09:07:29 +0900

    SortingStudent

---
+++ CodingTest/Sorting/SortingStudent.java
@@ -0,0 +1,57 @@
+package Sorting;
+
+import java.util.*;
+
+class Student implements Comparable<Student>{
+	
+	private String name;
+	private int score;
+	
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	@Override
+	public int compareTo(Student other) {
+		if(this.score < other.score ) {
+			return -1;
+		}
+		
+		return 1;
+	}
+}
+
+public class SortingStudent {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		List<Student> students = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++) {
+			String name = sc.next();
+			int score = sc.nextInt();
+			students.add(new Student(name, score));
+		}
+		
+		Collections.sort(students);
+		
+		for(int i = 0; i < students.size(); i++) {
+			System.out.println(students.get(i).getName() + " ");
+		}
+
+	}
+
+}
