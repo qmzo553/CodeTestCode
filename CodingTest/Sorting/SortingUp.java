00c947c76470073e2ff0bcb330fa9a2b1e0dc15d
박희준pc <pc@172.30.1.53>
2023-01-26 09:07:18 +0900

    SortingUp

---
+++ CodingTest/Sorting/SortingUp.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+import java.util.*;
+
+public class SortingUp {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		Integer[] arr = new Integer[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+}
