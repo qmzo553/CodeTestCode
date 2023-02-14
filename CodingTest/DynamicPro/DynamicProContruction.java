d7f5913dc1590faf4198c001d2865ecf37f369f4
qmzo553 <qmzo553@gmail.com>
2023-02-15 07:55:30 +0900

    DynamicProConstruction

---
+++ CodingTest/DynamicPro/DynamicProContruction.java
@@ -0,0 +1,23 @@
+package DynamicPro;
+
+import java.util.*;
+
+public class DynamicProContruction {
+	
+	public static int[] d = new int[1001];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		d[1] = 1;
+		d[2] = 3;
+		for(int i = 3; i <= n; i++) {
+			d[i] = (d[i - 1] + 2 * d[i - 2]) % 796796;
+		}
+		
+		System.out.println(d[n]);
+	}
+
+}
