fb46009dc09d17343aef5563e9896a853e146afc
qmzo553 <qmzo553@gmail.com>
2023-02-15 06:56:30 +0900

    SearchRepeatBinary

---
+++ CodingTest/Search/SearchRepeatBinary.java
@@ -0,0 +1,36 @@
+package Search;
+
+import java.util.*;
+
+public class SearchRepeatBinary {
+	
+	public static int binarySearch2(int[] arr, int target, int start, int end) {
+		while(start <= end) {
+			int mid = (start + end) / 2;
+			if (arr[mid] == target) return mid;
+			else if (arr[mid] > target) end = mid - 1;
+			else start = mid + 1;
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int target = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int result = binarySearch2(arr, target, 0, n - 1);
+		if(result == -1) {
+			System.out.println("원소가 존재하지 않습니다.");
+		} else {
+			System.out.println(result + 1);
+		}
+	}
+
+}
