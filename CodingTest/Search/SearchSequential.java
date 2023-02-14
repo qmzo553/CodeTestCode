b42442d4f16b5d6b30539bfcffa9a7c69d9bd41e
qmzo553 <qmzo553@gmail.com>
2023-02-14 22:33:27 +0900

    SearchSequantial

--- CodingTest/Search/SearchSequential.java
+++ CodingTest/Search/SearchSequential.java
@@ -1,9 +1,35 @@
 package Search;
 
+import java.util.*;
+
 public class SearchSequential {
 
+	public static int sequantialSearch(int n, String target, String[] arr) {
+		for(int i = 0; i < n; i++) {
+			System.out.println(arr[i]);
+			
+			if(arr[i].equals(target)) {
+				return i + 1;
+			}
+		}
+		return -1;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("생성할 원소 개수를 입력한 다음 한 칸 띄고 찾을 문자열을 입력하세요.");
+		
+		int n = sc.nextInt();
+		String target = sc.next();
+		
+		System.out.println("앞서 적은 원소 개수만큼 문자열을 입력하세요. 구분은 띄어쓰기 한 칸으로 합니다.");
+		String[] arr = new String[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.next();
+		}
+		
+		System.out.println(sequantialSearch(n, target, arr));
 
 	}
 
