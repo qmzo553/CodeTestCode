1eff83d2e6fbe192e5df8ce0ff6ad49241dcd9c2
qmzo553 <qmzo553@gmail.com>
2023-02-15 07:20:57 +0900

    SearchTTeock

---
+++ CodingTest/Search/SearchTTeok.java
@@ -0,0 +1,40 @@
+package Search;
+
+import java.util.*;
+
+public class SearchTTeok {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int start = 0;
+		int end = (int) 1e09;
+		int result = 0;
+		
+		while(start <= end) {
+			long total = 0;
+			int mid = (start + end) / 2;
+			for(int i = 0; i < n; i++) {
+				if(arr[i] > mid) total += arr[i] - mid;
+			}
+			if(total < m) {
+				end = mid - 1;
+			} else {
+				result = mid;
+				start = mid + 1;
+			}
+		}
+		
+		System.out.println(result);
+
+	}
+
+}
