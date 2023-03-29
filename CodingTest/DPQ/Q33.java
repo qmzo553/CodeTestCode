5a42484329f23c4e7b4e452fedd9d7f880bbd54a
qmzo553 <qmzo553@gmail.com>
2023-03-29 22:13:28 +0900

    3.29

--- CodingTest/DPQ/Q33.java
+++ CodingTest/DPQ/Q33.java
@@ -1,10 +1,37 @@
 package DPQ;
 
+import java.util.*;
+
 public class Q33 {
 
+	static int n;
+	static int[] t = new int[15];
+	static int[] p = new int[15];
+	static int[] dp = new int[16];
+	static int maxValue;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			t[i] = sc.nextInt();
+			p[i] = sc.nextInt();
+		}
+		
+		for(int i = n - 1; i >= 0; i--) {
+			int time = t[i] + i;
+			
+			if(time <= n) {
+				dp[i] = Math.max(p[i] + dp[time], maxValue);
+				maxValue = dp[i];
+			}
+			
+			else dp[i] = maxValue;
+		}
+		
+		System.out.println(maxValue);
 	}
 
 }
