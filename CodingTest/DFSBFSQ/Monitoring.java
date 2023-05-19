d67eed862ac4dacb829f7dded4feb20fdc6268d5
qmzo553 <qmzo553@gmail.com>
2023-05-19 13:39:54 +0900

    5.19

--- CodingTest/DFSBFSQ/Monitoring.java
+++ CodingTest/DFSBFSQ/Monitoring.java
@@ -1,158 +1,93 @@
 package DFSBFSQ;
 
 import java.util.*;
+import java.io.*;
 
 class Combination {
 	
-	private int n;
-	private int r;
-	private int[] now;
-	private ArrayList<ArrayList<Position>> result;
+	int n, r;
+	int[] now;
+	ArrayList<ArrayList<Position>> result;
 	
 	public ArrayList<ArrayList<Position>> getResult() {
+		
 		return result;
 	}
 	
 	public Combination(int n, int r) {
+		
 		this.n = n;
 		this.r = r;
 		this.now = new int[r];
 		this.result = new ArrayList<ArrayList<Position>>();
 	}
 	
-	public void combination(ArrayList<Position> arr, int depth, int index, int target) {
+	public void combination(ArrayList<Position> arr, int depth, int index, int target ) {
 		
 		if(depth == r) {
+			
 			ArrayList<Position> temp = new ArrayList<>();
 			for(int i = 0; i < now.length; i++) {
+				
 				temp.add(arr.get(now[i]));
 			}
 			
 			result.add(temp);
 			return;
 		}
+		
 		if(target == n) return;
 		now[index] = target;
-		combination(arr, depth + 1, index + 1, target - 1);
+		combination(arr, depth + 1, index + 1, target + 1);
 		combination(arr, depth, index, target + 1);
-	}		
+	}
 }
 
 class Position {
 	
-	private int x;
-	private int y;
+	int x, y;
 	
 	public Position(int x, int y) {
+		
 		this.x = x;
 		this.y = y;
 	}
 	
 	public int getX() {
+		
 		return this.x;
 	}
 	
 	public int getY() {
+		
 		return this.y;
 	}
 }
 
 public class Monitoring {
 	
-	public static int n;
-	public static char[][] board = new char[6][6];
+	public static int N;
+	public static char[][] map;
 	public static ArrayList<Position> teachers = new ArrayList<>();
 	public static ArrayList<Position> spaces = new ArrayList<>();
 	
-	public static boolean watch(int x, int y, int direction) {
+	public static void main(String[] args) throws IOException {
 		
-		if(direction == 0) {
-			while(y >= 0) {
-				if(board[x][y] == 'S') {
-					return true;
-				}
-				if(board[x][y] == 'O') {
-					return false;
-				}
-				
-				y -= 1;
-			}
-		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		if(direction == 1) {
-			while(y < n) {
-				if(board[x][y] == 'S') {
-					return true;
-				}
-				if(board[x][y] == 'O') {
-					return false;
-				}
-				
-				y += 1;
-			}
-		}
+		N = Integer.parseInt(br.readLine());
+		map = new char[N][N];
 		
-		if(direction == 2) {
-			while(x >= 0) {
-				if(board[x][y] == 'S') {
-					return true;
-				}
-				if(board[x][y] == 'O') {
-					return false;
-				}
-				
-				x -= 1;
-			}
-		}
-		
-		if(direction == 3) {
-			while(x < n) {
-				if(board[x][y] == 'S') {
-					return true;
-				}
-				if(board[x][y] == 'O') {
-					return false;
-				}
-				
-				x += 1;
-			}
-		}
-		
-		return false;
-	}
-	
-	public static boolean process() {
-		for(int i = 0; i < teachers.size(); i++) {
+		for(int i = 0; i < N; i++) {
 			
-			int x = teachers.get(i).getX();
-			int y = teachers.get(i).getY();
-			
-			for(int j = 0; j < 4; j++) {
-				if(watch(x, y, j)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				board[i][j] = sc.next().charAt(0);
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
 				
-				if(board[i][j] == 'T') {
-					teachers.add(new Position(i, j));
-				}
+				map[i][j] = st.nextToken().charAt(0);
 				
-				if(board[i][j] == 'X') {
-					spaces.add(new Position(i, j));
-				}
+				if(map[i][j] == 'T') teachers.add(new Position(i, j));
+				if(map[i][j] == 'X') spaces.add(new Position(i, j));
 			}
 		}
 		
@@ -162,26 +97,97 @@ public class Monitoring {
 		
 		boolean found = false;
 		for(int i = 0; i < spaceList.size(); i++) {
+			
 			for(int j = 0; j < spaceList.get(i).size(); j++) {
+				
 				int x = spaceList.get(i).get(j).getX();
 				int y = spaceList.get(i).get(j).getY();
-				board[x][y] = 'O';
+				map[x][y] = 'O';
 			}
 			
 			if(!process()) {
+				
 				found = true;
 				break;
 			}
 			
 			for(int j = 0; j < spaceList.get(i).size(); j++) {
+				
 				int x = spaceList.get(i).get(j).getX();
-                int y = spaceList.get(i).get(j).getY();
-                board[x][y] = 'X';
+				int y = spaceList.get(i).get(j).getY();
+				map[x][y] = 'X';
 			}
 		}
 		
 		if(found) System.out.println("YES");
 		else System.out.println("NO");
 	}
-
+	
+	public static boolean process() {
+		
+		for(int i = 0; i < teachers.size(); i++) {
+			
+			int x = teachers.get(i).getX();
+			int y = teachers.get(i).getY();
+			
+			for(int j = 0; j < 4; j++) {
+				
+				if(watch(x, y, j)) {
+					
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	public static boolean watch(int x, int y, int direction) {
+		
+		if(direction == 0) {
+			
+			while(y >= 0) {
+				
+				if(map[x][y] == 'S') return true;
+				if(map[x][y] == 'O') return false;
+				
+				y -= 1;
+			}
+		}
+		
+		if(direction == 1) {
+			
+			while(y < N) {
+				
+				if(map[x][y] == 'S') return true;
+				if(map[x][y] == 'O') return false;
+				
+				y += 1;
+			}
+		}
+		
+		if(direction == 2) {
+			
+			while(x >= 0) {
+				
+				if(map[x][y] == 'S') return true;
+				if(map[x][y] == 'O') return false;
+				
+				x -= 1;
+			}
+		}
+		
+		if(direction == 3) {
+			
+			while(x < N) {
+				
+				if(map[x][y] == 'S') return true;
+				if(map[x][y] == 'O') return false;
+				
+				x += 1;
+			}
+		}
+		
+		return false;
+	}
 }
--- CodingTest/DFSBFSQ/Operator.java
+++ CodingTest/DFSBFSQ/Operator.java
@@ -5,10 +5,72 @@ import java.io.*;
 
 public class Operator {
 	
+	public static int N;
+	public static ArrayList<Integer> arr = new ArrayList<>();
+	public static int add, sub, mul, divi;
+	public static int min = Integer.MAX_VALUE;
+	public static int max = Integer.MIN_VALUE;
 	
 	public static void main(String[] args) throws IOException {
 	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			
+			arr.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		add = Integer.parseInt(st.nextToken());
+		sub = Integer.parseInt(st.nextToken());
+		mul = Integer.parseInt(st.nextToken());
+		divi = Integer.parseInt(st.nextToken());
+		
+		dfs(1, arr.get(0));
+		
+		System.out.println(max);
+		System.out.println(min);
+	}
+	
+	public static void dfs(int i, int now) {
+		
+		if(i == N) {
+			
+			min = Math.min(min, now);
+			max = Math.max(max, now);
+		} else {
+			
+			if(add > 0) {
+				
+				add -= 1;
+				dfs(i + 1, now + arr.get(i));
+				add += 1;
+			}
+			
+			if(sub > 0) {
+				
+				sub -= 1;
+				dfs(i + 1, now - arr.get(i));
+				sub += 1;
+			}
+			
+			if(mul > 0) {
+				
+				mul -= 1;
+				dfs(i + 1, now * arr.get(i));
+				mul += 1;
+			}
+			
+			if(divi > 0) {
+				
+				divi -= 1;
+				dfs(i + 1, now / arr.get(i));
+				divi += 1;
+			}
+		}
 	}
 
 }
